f4c30ca94bfc3e3695475ce07ebb706977686f7f
iniiyan36 <iniiyan36@gmail.com>
2025-12-30 20:32:28 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.list');
+
+document.body.appendChild(navList);
+
 hamburger.addEventListener('click', () => {
     navList.classList.toggle('active');
 });

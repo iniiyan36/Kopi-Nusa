fe3976e40b84121257030fd904b7f461aa47cc27
iniiyan36 <iniiyan36@gmail.com>
2025-12-30 19:03:51 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.list');
+hamburger.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});

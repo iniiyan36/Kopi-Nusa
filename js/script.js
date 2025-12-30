2792f6aa3ec95f706ad0b11bb7c61f7a49b2394a
iniiyan36 <iniiyan36@gmail.com>
2025-12-30 20:46:02 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.list');
 
 hamburger.addEventListener('click', () => {
-    navList.classList.toggle('active');
+  navList.classList.toggle('active');
+
+  // lock scroll saat menu aktif
+  document.body.style.overflow =
+    navList.classList.contains('active') ? 'hidden' : '';
 });
